3bc294e475f04823222e3f31ddc01515eb345c3d
sergiuchilat <chilatsergiu@gmail.com>
2021-09-07 09:05:10 +0300

    Initialize "name" property using constructor

--- src/hello/Transport.java
+++ src/hello/Transport.java
@@ -3,10 +3,12 @@ package hello;
 public class Transport {
     private String name;
     Transport(){
+        this.name = "Noname";
         System.out.println("I am a Transport");
     }
 
     Transport(String name){
+        this.name = name;
         System.out.println("I am a " + name);
     }
 }
