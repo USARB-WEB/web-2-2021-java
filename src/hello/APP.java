5516c0621d5c619d1f8f4909f9ca639be1f341e5
sergiuchilat <chilatsergiu@gmail.com>
2021-09-07 09:02:26 +0300

    Transport private property "name"

--- src/hello/APP.java
+++ src/hello/APP.java
@@ -3,5 +3,6 @@ package hello;
 public class APP {
     public static void main(String[] args) {
         Transport transport = new Transport();
+        Transport t2 = new Transport("Bus");
     }
 }
--- src/hello/Transport.java
+++ src/hello/Transport.java
@@ -1,7 +1,12 @@
 package hello;
 
 public class Transport {
+    private String name;
     Transport(){
         System.out.println("I am a Transport");
+    }
+
+    Transport(String name){
+        System.out.println("I am a " + name);
     }
 }
