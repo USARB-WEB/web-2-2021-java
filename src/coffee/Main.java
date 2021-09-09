ad9c8eb2ff8072e28d2209fe7ecf70420e87863b
sergiuchilat <chilatsergiu@gmail.com>
2021-09-09 10:15:44 +0300

    Abstraction: call method choose

--- src/coffee/Main.java
+++ src/coffee/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         CoffeeMachine coffeeMachine = new CoffeeMachine();
         coffeeMachine.pay();
+        coffeeMachine.choose();
     }
 }
